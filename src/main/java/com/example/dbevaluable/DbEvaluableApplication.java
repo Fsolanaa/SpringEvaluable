686cc6890d27d46839b5608ea9ba157c5d0eeb6b
Paco Solana <fsolanaaranega@gmail.com>
2025-02-22 18:16:38 +0100

    Creación proyecto

---
+++ src/main/java/com/example/dbevaluable/DbEvaluableApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbevaluable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbEvaluableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbEvaluableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dbevaluable/DbEvaluableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbevaluable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbEvaluableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
